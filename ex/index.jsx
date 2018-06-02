189d5d5f986a9abdd5fe023759e6a62ecb33f901
Guilherme de Jesus Santos <guiljs@gmail.com>
2018-06-02 01:01:58 -0300

    Usando as propriedades do componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Componente from './componente'
 
-ReactDOM.render(<Componente />, document.getElementById('app'))
+ReactDOM.render(<Componente value="Show!" />, document.getElementById('app'))
